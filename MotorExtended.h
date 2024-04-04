f3d66cac32d184ca7b1bb4916512557f415a8794
Baxter Gonzalez <57194018+baxtergonzalez@users.noreply.github.com>
2024-04-04 17:17:27 -0500

    Added square navigation functions
    Here you go

---
+++ MotorExtended.h
@@ -0,0 +1,37 @@
+#ifndef MOTOREXTENDED_H
+#define MOTOREXTENDED_H
+
+#include <Wire.h>
+#include <AS5600.h>
+#include <TB6612_ESP32.h>
+
+class MotorExtended {
+public:
+    // Constructor declarations
+    MotorExtended(int IN1, int IN2, int PWM, int STBY_PIN, int CHANNEL, TwoWire& wire, int DIRECTION = 1);
+    MotorExtended(int IN1, int IN2, int PWM, int STBY_PIN, int CHANNEL, int DIRECTION = 1);
+
+    void connectEncoder();
+    void updateEncoderPos();
+    void analogDriveMotor(int analogSpeed);
+    void brakeMotor();
+    void encoderDrive(long delta); // Drive the motor based on delta encoder counts
+
+    long cumulative_position = 0;
+    long previous_cumulative_position = 0;
+    long offset_cumulative_position = 0;
+    bool seeking_target = false; // Whether the motor is currently seeking a target position
+    int prev_error;
+
+private:
+    Motor motor;
+    AS5600 encoder;
+    long target_position = 0; // Target encoder position to reach
+    float kP = 0.1; // Proportional gain for encoderDrive
+    int analog_velocity = 0;
+    long prev_read_time = 0;
+
+    void proportionalControl(); // Adjusts motor speed based on the proportional control logic
+};
+
+#endif
---
+++ Robot.h
@@ -0,0 +1,45 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include "MotorExtended.h"
+#include "DistanceSensor.h"
+
+class Robot {
+public:
+    Robot(int ain1, int ain2, int pwma, int stby, int bin1, int bin2, int pwmb, int dist_pin, TwoWire& i2c);
+
+    void updateAll();
+    void moveForward(int speed);
+    void moveReverse();
+    void reverseFromLeft();
+    void reverseFromRight();
+    void turnLeft();
+    void turnRight();
+    void scanLeftandRight();
+    void brakeMotors();
+    void getClearOfObject();
+    void seedRound();
+    void linearDrive(int ticks);
+    void turn90Degrees(int direction);
+    void forwardOneSquare();
+    void leftOneSquare();
+    void rightOneSquare();
+    void testRound();
+    void driveInSquare(float distance);
+
+
+    MotorExtended motor1;
+    MotorExtended motor2;
+    DistanceSensor dist1;
+
+private:
+    int distPin;
+    TwoWire& i2c;
+    int targetTicks = 0; // Default initialization
+    bool isTraveling = false; // Default initialization
+    int square_len = 4; //Each square in occupancy grid is 4x4 inches
+
+    long distToEncoderTicks(float distance);
+};
+
+#endif
